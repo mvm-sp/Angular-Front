f8964ef536e0cb96a5d3251512765caad63a504a
mvm-sp <43617650+mvm-sp@users.noreply.github.com>
2019-08-23 19:40:24 -0300

    Files

---
+++ Angular6/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { MyTableComponent } from './my-table/my-table.component';
+
+const appRoutes: Routes = [
+  { path: 'first-page', component: FirstPageComponent},
+  { path: 'second-page', component: MyDashboardComponent},
+  { path: 'third-page', component: MyTableComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyNavComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent,
+    MyDashboardComponent,
+    MyTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
